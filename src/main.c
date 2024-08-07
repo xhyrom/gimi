7e9d29d8066469a501308064b4d632c1f702b4c3
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-08-07 22:47:09 +0200

    fix: return exit code 1 without args

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 int main(int argc, char **argv) {
   if (argc == 1) {
     cli_print_help();
-    return 0;
+    return 1;
   }
 
   if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
